5229acd56c0c09cec2a51ced5f9141529890fec7
ikzylol <djikervicente@gmail.com>
2025-03-22 21:04:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.get("/api/valorant-stats", async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
+
